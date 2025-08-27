2f6a117d77105692b61b529cc378bba02e3f2a7b
lvnvn177 <53424393+lvnvn177@users.noreply.github.com>
2025-08-27 12:16:38 +0900

    init

---
+++ src/main/java/com/burgerking/membership/vip_batch_optimizer/VipBatchOptimizerApplication.java
@@ -0,0 +1,13 @@
+package com.burgerking.membership.vip_batch_optimizer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VipBatchOptimizerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VipBatchOptimizerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/burgerking/membership/vip_batch_optimizer/VipBatchOptimizerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.burgerking.membership.vip_batch_optimizer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VipBatchOptimizerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
