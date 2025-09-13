cf2acb7d4f50076ccdb05b869b3765dbe509b403
lvnvn177 <53424393+lvnvn177@users.noreply.github.com>
2025-09-14 02:20:38 +0900

    주석 에러 해결

--- src/main/java/com/burgerking/common/config/WebConfig.java
+++ src/main/java/com/burgerking/common/config/WebConfig.java
@@ -12,7 +12,7 @@ public class WebConfig implements WebMvcConfigurer {
 
     /**
      * 전역 CORS(Cross-Origin Resource Sharing) 설정을 추가합니다.
-     * - 특정 Originからのリクエストを許可します。
+     * - 특정 Origin 주소 지정 
      * - 허용할 HTTP 메서드와 헤더를 지정합니다.
      *
      * @param registry CORS 설정을 등록하는 데 사용됩니다.
