579f20304e4630acce585a51824e66353cf16ebb
lvnvn177 <53424393+lvnvn177@users.noreply.github.com>
2025-09-14 22:00:12 +0900

    Entitiy 리포지토리 명시

--- src/main/java/com/burgerking/membership/batch/config/MembershipGradeBatchConfig.java
+++ src/main/java/com/burgerking/membership/batch/config/MembershipGradeBatchConfig.java
@@ -15,7 +15,7 @@ import org.springframework.batch.item.database.JpaPagingItemReader;
 import org.springframework.batch.item.database.builder.JpaPagingItemReaderBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
+import org.springframework.beans.factory.annotation.Qualifier;
 
 import org.springframework.batch.core.job.builder.JobBuilder;
 import org.springframework.batch.core.repository.JobRepository;
@@ -37,6 +37,8 @@ public class MembershipGradeBatchConfig {
 
     private final JobRepository jobRepository;
     private final PlatformTransactionManager transactionManager;
+
+    @Qualifier("membershipEntityManagerFactory")
     private final EntityManagerFactory entityManagerFactory;
     private final MembershipRepository membershipRepository;
     private final MonthlyOrderRepository monthlyOrderRepository;
