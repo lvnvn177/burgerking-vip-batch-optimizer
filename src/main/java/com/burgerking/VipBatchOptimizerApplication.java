ddd372e5e6ccf459c99472243418d385b9bfc8c7
lvnvn177 <53424393+lvnvn177@users.noreply.github.com>
2025-09-14 22:40:05 +0900

    데이터 소스 명시

--- src/main/java/com/burgerking/VipBatchOptimizerApplication.java
+++ src/main/java/com/burgerking/VipBatchOptimizerApplication.java
@@ -1,11 +1,17 @@
 package com.burgerking;
 
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @EnableJpaAuditing
-@SpringBootApplication
+@EnableBatchProcessing(
+    dataSourceRef = "membershipDataSource",
+    transactionManagerRef = "membershipTransactionManager"
+)
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class VipBatchOptimizerApplication {
 
     public static void main(String[] args) {
